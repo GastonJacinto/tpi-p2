77ba409275fc595a512bf3fbc8ec6bb0a02d8662
Gastón <gastibarossi@gmail.com>
2025-11-16 16:05:40 -0300

    chore: add database connection file

--- src/main/java/config/DatabaseConnection.java
+++ src/main/java/config/DatabaseConnection.java
@@ -7,7 +7,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/tfi-p2?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASS = "gasti2000";
+    private static final String PASS = "";
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASS);
