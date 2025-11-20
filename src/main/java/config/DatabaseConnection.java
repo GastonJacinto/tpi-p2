625a46831e5b61f125c83ab50ffe056892cd7de5
YouWannaCry <arodrigoegea@gmail.com>
2025-11-19 22:19:35 -0300

    tpi-p2 [FEAT] Se corrigieron algunos issues, ademas se cambio la conexion de la db

--- src/main/java/config/DatabaseConnection.java
+++ src/main/java/config/DatabaseConnection.java
@@ -5,9 +5,18 @@ import java.sql.DriverManager;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/tfi-p2?useSSL=false&serverTimezone=UTC";
+
+    private static final String URL = "jdbc:mysql://localhost:3306/tpip2?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASS = "gasti2000";
+    private static final String PASS = "root";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("No se pudo cargar el driver MySQL", e);
+        }
+    }
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASS);
